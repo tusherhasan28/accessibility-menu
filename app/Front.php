f6612fa3cdf76f6b0963b1ec927484ca57747ca0
Habibur Rahman <habibblog97@gmail.com>
2024-01-13 10:35:22 +0600

    front assets added

--- app/Front.php
+++ app/Front.php
@@ -8,7 +8,19 @@ class Front {
      * Class Constructor
      */
     public function __construct() {
-        
+        add_action( 'wp_enqueue_scripts', [$this, 'enque_scripts'] );
     }
+
+    /**
+     * load all fornt assets
+     */
+    public function enque_scripts() {
+        
+        if( ! is_admin() ) {
+            wp_enqueue_style( 'front', AS_ASSET . '/front/css/front.css', '', time(), 'all' );
+
+            wp_enqueue_script( 'front', AS_ASSET . '/front/js/front.js', ['jquery'], time(), true );
+        }
+    } 
 
 }
