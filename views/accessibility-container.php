cc37e36d4570ac1ed65b9ab7722a6b83f46dd565
Habibur Rahman <habibblog97@gmail.com>
2024-01-13 11:30:25 +0600

    heading done

--- views/accessibility-container.php
+++ views/accessibility-container.php
@@ -1,3 +1,15 @@
 <div class="as-container">
-    
+    <div class="as-heading">
+        <div class="as-heading-title">
+            <h3>Accessibility Menu</h3>
+        </div>
+        <div id="as-cross" class="as-heading-cross">
+            <img src="<?php esc_html_e( AS_ASSET . '/img/close.png' ); ?>">
+        </div>
+    </div>
+    <div class="as-wrapper">
+        <div class="as-single-item">
+
+        </div>
+    </div>
 </div>
