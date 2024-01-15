9a14a06e997ec59c92ffabb6f7448b8a7eba9746
Habibur Rahman <habibblog97@gmail.com>
2024-01-15 22:05:03 +0600

    text spacing added

--- assets/front/js/front.js
+++ assets/front/js/front.js
@@ -22,21 +22,19 @@ jQuery(function($){
      */
     $(document).on('click', '.contrast', function(){
 
-        var body = $('body');
+        var body = $("body *").not(".as-container");
 
         var isContrasted = body.hasClass('contrasted');
 
         if (isContrasted) {
-            body.css({"background-color": ""}).removeClass('contrasted');
-            $("p, h1, h2, h3, h4, h5, h6, span, div").css('color', '');
+            body.css({"background-color": "", "color": ""}).removeClass('contrasted');
         } else {
-            body.css({"background-color": "black"}).addClass('contrasted');
-            $("p, h1, h2, h3, h4, h5, h6, span, div").css('color', 'blue');
+            body.css({"background-color": "black", "color": "blue"}).addClass('contrasted');
         }
     });
     $(document).on('click', '.hightlight-links', function(){
 
-        var links = $('a');
+        var links = $('body:not(.as-container) a');
 
         var isHighlighted = links.hasClass('highlighted');
 
@@ -47,32 +45,34 @@ jQuery(function($){
         }
     });
 
-    var isIncreased = false;
-    $(document).on('click', '.bigger-text', function(){
+    // var isIncreased = false;
+    // $(document).on('click', '.bigger-text', function(){
 
-        if (isIncreased) {
-            // If font size is increased, decrease it by dividing by 2
-            $("body, *").css("font-size", "inherit");
-          } else {
-            // If font size is not increased, increase it by multiplying by 2
-            $("body, *").css("font-size", function(index, currentSize) {
-              return parseFloat(currentSize) * 2 + "px";
-            });
-        }
-    });
-    isIncreased = !isIncreased; 
+    //     if (isIncreased) {
+    //         // If font size is increased, decrease it by dividing by 2
+    //         $("body, *").css("font-size", "inherit");
+    //       } else {
+    //         // If font size is not increased, increase it by multiplying by 2
+    //         $("body, *").css("font-size", function(index, currentSize) {
+    //           return parseFloat(currentSize) * 2 + "px";
+    //         });
+    //     }
+    // });
+    // isIncreased = !isIncreased; 
 
     $(document).on('click', '.text-spacing', function(){
 
-        var body = $('body');
+        textSpecingBody = $('body');
+        var body = $('body *').not('.as-container, .as-container *');
 
-        var isTextSpacing = body.hasClass('textSpacing');
+        var isTextSpacing = textSpecingBody.hasClass('textSpacing');
 
         if (isTextSpacing) {
-            $("p, h1, h2, h3, h4, h5, h6, span, div").css('letter-spacing', '');
+            body.css('letter-spacing', '');
+            textSpecingBody.removeClass('textSpacing');
         } else {
-            $("p, h1, h2, h3, h4, h5, h6, span, div").css('letter-spacing', '3px');
-            body.addClass('textSpacing');
+            body.css('letter-spacing', '3px');
+            textSpecingBody.addClass('textSpacing');
         }
         
     });
