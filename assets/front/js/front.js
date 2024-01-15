f08de966897c95c280243ab38bba78662f19281a
Habibur Rahman <habibblog97@gmail.com>
2024-01-15 20:54:59 +0600

    constrast added

--- assets/front/js/front.js
+++ assets/front/js/front.js
@@ -21,7 +21,18 @@ jQuery(function($){
      * For action all menu item
      */
     $(document).on('click', '.contrast', function(){
-        $('body').css({"background-color": "black", "color": "yellow"});
+
+        var body = $('body');
+
+        var isContrasted = body.hasClass('contrasted');
+
+        if (isContrasted) {
+            body.css({"background-color": ""}).removeClass('contrasted');
+            $("p, h1, h2, h3, h4, h5, h6, span, div").css('color', '');
+        } else {
+            body.css({"background-color": "black"}).addClass('contrasted');
+            $("p, h1, h2, h3, h4, h5, h6, span, div").css('color', 'blue');
+        }
     });
     $(document).on('click', '.hightlight-links', function(){
 
