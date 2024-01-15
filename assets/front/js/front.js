27427a45c42b44a8bdfc97367f5840f1debcd6ce
Sanzida Tasnim <sanzidatasnim@Sanzidas-MacBook-Air.local>
2024-01-16 00:59:55 +0600

    upto cursor

--- assets/front/js/front.js
+++ assets/front/js/front.js
@@ -1,71 +1,116 @@
-jQuery(function($){
-    /**
-     * Show and hide Accessibility
-     */
-    $(document).on('click', '#as-main', function(){
-        $('#as-container').show();
-    });
-    $(document).on('click', '#as-cross', function(){
-        $('#as-container').hide();
-    });
+jQuery(function ($) {
+  /**
+   * Show and hide Accessibility
+   */
+  $(document).on('click', '#as-main', function () {
+    $('#as-container').show();
+  });
+  $(document).on('click', '#as-cross', function () {
+    $('#as-container').hide();
+  });
 
-    /**
-     * Click Single Item
-     */
-    $(document).on('click', '.as-single-item', function(){
-        $(this).toggleClass('active');
-    });
+  /**
+   * Click Single Item
+   */
+  $(document).on('click', '.as-single-item', function () {
+    $(this).toggleClass('active');
+  });
 
-    
-    /**
-     * For action all menu item
-     */
-    //Constrast
-    $(document).on('click', '.contrast', function(){
+  /**
+   * For action all menu item
+   */
+  //Constrast
+  $(document).on('click', '.contrast', function () {
+    let constrastBody = $('body');
+    let body = $('body *').not('.as-container, .as-container *');
 
-        var constrastBody = $('body');
-        var body = $('body *').not('.as-container, .as-container *');
+    let isContrasted = constrastBody.hasClass('contrasted');
 
-        var isContrasted = constrastBody.hasClass('contrasted');
+    if (isContrasted) {
+      body.css({ 'background-color': '', color: '' });
+      constrastBody.removeClass('contrasted');
+    } else {
+      body.css({ 'background-color': 'black', color: 'blue' });
+      constrastBody.addClass('contrasted');
+    }
+  });
 
-        if (isContrasted) { 
-            body.css({"background-color": "", "color": ""});
-            constrastBody.removeClass('contrasted');
-        } else {
-            body.css({"background-color": "black", "color": "blue"});
-            constrastBody.addClass('contrasted');
-        }
-    });
+  //Hightlight links
+  $(document).on('click', '.hightlight-links', function () {
+    let links = $('a');
 
-    //Hightlight links
-    $(document).on('click', '.hightlight-links', function(){
+    let isHighlighted = links.hasClass('highlighted');
 
-        var links = $('a');
+    if (isHighlighted) {
+      links
+        .css({ 'background-color': '', color: '' })
+        .removeClass('highlighted');
+    } else {
+      links
+        .css({ 'background-color': 'black', color: 'yellow' })
+        .addClass('highlighted');
+    }
+  });
 
-        var isHighlighted = links.hasClass('highlighted');
+  //Text Spacing
+  $(document).on('click', '.text-spacing', function () {
+    let textSpecingBody = $('body');
+    let body = $('body *').not('.as-container, .as-container *');
 
-        if (isHighlighted) {
-            links.css({"background-color": "", "color": ""}).removeClass('highlighted');
-        } else {
-            links.css({"background-color": "black", "color": "yellow"}).addClass('highlighted');
-        }
-    });
+    let isTextSpacing = textSpecingBody.hasClass('textSpacing');
 
-    //Text Spacing
-    $(document).on('click', '.text-spacing', function(){
+    if (isTextSpacing) {
+      body.css('letter-spacing', '');
+      textSpecingBody.removeClass('textSpacing');
+    } else {
+      body.css('letter-spacing', '3px');
+      textSpecingBody.addClass('textSpacing');
+    }
+  });
 
-        textSpecingBody = $('body');
-        var body = $('body *').not('.as-container, .as-container *');
+  //Bigger Text
+  $(document).on('click', '.biggerText', function () {
+    let biggerTextTag = $('p , h1');
+    let body = $('body *').not('.as-container, .as-container *');
 
-        var isTextSpacing = textSpecingBody.hasClass('textSpacing');
+    let isBiggerText = biggerTextTag.hasClass('biggerText');
 
-        if (isTextSpacing) {
-            body.css('letter-spacing', '');
-            textSpecingBody.removeClass('textSpacing');
-        } else {
-            body.css('letter-spacing', '3px');
-            textSpecingBody.addClass('textSpacing');
-        }
-        
-    });
+    if (isBiggerText) {
+      body.css('font-size', '');
+      biggerTextTag.removeClass('biggerText');
+    } else {
+      body.css('font-size', '30px');
+      biggerTextTag.addClass('biggerText');
+    }
+  });
+
+  //Hide Image
+  $(document).on('click', '.hideImages', function () {
+    let imgTag = $('img');
+    let body = $('body *').not('.as-container, .as-container *');
+
+    let isHideImage = imgTag.hasClass('hideImages');
+
+    if (isHideImage) {
+      imgTag.css('visibility', 'visible');
+      imgTag.removeClass('hideImages');
+    } else {
+      imgTag.css('visibility', 'hidden');
+      imgTag.addClass('hideImages');
+    }
+  });
+  // Bigger Cursor
+  $(document).on('click', '.biggerCursor', function () {
+    let body = $('body *').not('.as-container, .as-container *');
+
+    let cursorBigger = body.hasClass('biggerCursor');
+
+    if (cursorBigger) {
+      body.css('cursor', 'auto');
+      body.removeClass('biggerCursor');
+    } else {
+      body.addClass('biggerCursorCss');
+      body.addClass('biggerCursor');
+    }
+  });
 });
--- views/accessibility-container.php
+++ views/accessibility-container.php
@@ -12,7 +12,7 @@
             <img src="<?php esc_html_e( AS_ASSET . '/img/download.svg' ); ?>">
             <p><?php esc_html_e( 'Contrast +', 'accessibility' ) ?> </p>
         </div>
-        <div class="as-single-item">
+        <div class="as-single-item smart-contrast">
             <img src="<?php esc_html_e( AS_ASSET . '/img/download.svg' ); ?>">
             <p><?php esc_html_e( 'Smart contrast', 'accessibility' ) ?> </p>
         </div>
@@ -20,7 +20,7 @@
             <img src="<?php esc_html_e( AS_ASSET . '/img/download.svg' ); ?>">
             <p><?php esc_html_e( 'Highlight Links', 'accessibility' ) ?> </p>
         </div>
-        <div class="as-single-item bigger-text">
+        <div class="as-single-item biggerText">
             <img src="<?php esc_html_e( AS_ASSET . '/img/download.svg' ); ?>">
             <p><?php esc_html_e( 'Bigger Text', 'accessibility' ) ?> </p>
         </div>
@@ -28,35 +28,31 @@
             <img src="<?php esc_html_e( AS_ASSET . '/img/download.svg' ); ?>">
             <p><?php esc_html_e( 'Text Spacing', 'accessibility' ) ?> </p>
         </div>
-        <div class="as-single-item">
+        <div class="as-single-item hideImages">
             <img src="<?php esc_html_e( AS_ASSET . '/img/download.svg' ); ?>">
             <p><?php esc_html_e( 'Hide images', 'accessibility' ) ?> </p>
         </div>
-        <div class="as-single-item">
-            <img src="<?php esc_html_e( AS_ASSET . '/img/download.svg' ); ?>">
-            <p><?php esc_html_e( 'Fonts', 'accessibility' ) ?> </p>
-        </div>
-        <div class="as-single-item">
+        <div class="as-single-item biggerCursor">
             <img src="<?php esc_html_e( AS_ASSET . '/img/download.svg' ); ?>">
             <p><?php esc_html_e( 'Bigger cursor', 'accessibility' ) ?> </p>
         </div>
-        <div class="as-single-item">
+        <div class="as-single-item line-height">
             <img src="<?php esc_html_e( AS_ASSET . '/img/download.svg' ); ?>">
             <p><?php esc_html_e( 'Line Height', 'accessibility' ) ?> </p>
         </div>
-        <div class="as-single-item">
+        <div class="as-single-item text-align">
             <img src="<?php esc_html_e( AS_ASSET . '/img/download.svg' ); ?>">
             <p><?php esc_html_e( 'Text align', 'accessibility' ) ?> </p>
         </div>
-        <div class="as-single-item">
+        <div class="as-single-item saturation">
             <img src="<?php esc_html_e( AS_ASSET . '/img/download.svg' ); ?>">
             <p><?php esc_html_e( 'Saturation', 'accessibility' ) ?> </p>
         </div>
-        <div class="as-single-item">
+        <div class="as-single-item virtual-selection">
             <img src="<?php esc_html_e( AS_ASSET . '/img/download.svg' ); ?>">
             <p><?php esc_html_e( 'Virtual selection', 'accessibility' ) ?> </p>
         </div>
-        <div class="as-single-item">
+        <div class="as-single-item testing">
             <img src="<?php esc_html_e( AS_ASSET . '/img/download.svg' ); ?>">
             <p><?php esc_html_e( 'Testing', 'accessibility' ) ?> </p>
         </div>
