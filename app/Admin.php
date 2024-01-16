f5d75e2775c34f53668354364cf269211b749953
Habibur Rahman <habibblog97@gmail.com>
2024-01-16 21:00:32 +0600

    menu added

--- app/Admin.php
+++ app/Admin.php
@@ -8,7 +8,31 @@ class Admin {
      * Class Constructor
      */
     public function __construct() {
-        
+        add_action( 'admin_menu', [$this, 'admin_menu'] );
+    }
+
+    /**
+     * Register admin menu
+     */
+    public function admin_menu() {
+        add_menu_page(
+            __( 'Accessibility Menu' ),
+            __( 'Accessibility Menu' ),
+            'manage_options', 
+            'accessibility-menu',
+            [$this, 'admin_page_content'],
+            'dashicons-universal-access-alt',
+            20
+        );
+    }
+
+    /**
+     * Accessibility Menu callback function 
+    */
+    public function admin_page_content() {
+        ?>
+            
+        <?php
     }
 
 }
